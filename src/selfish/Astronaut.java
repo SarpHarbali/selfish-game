7275494171fd2dff42559d06baf13869707f2678
j68216uh <umut.harbalioglu@student.manchester.ac.uk>
2023-04-26 23:48:49 +0100

    first classes

---
+++ src/selfish/Astronaut.java
@@ -0,0 +1,11 @@
+package selfish;
+
+public class Astronaut {
+    private String name;
+    private GameEngine game;
+
+    public Astronaut(String nameIn, GameEngine gameIn) {
+        name = nameIn;
+        game = gameIn;
+    }
+}
