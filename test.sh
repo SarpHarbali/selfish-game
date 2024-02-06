98abb680969eec79e916d018b0881482fb2f311e
SarpHarbali <87227500+SarpHarbali@users.noreply.github.com>
2024-02-06 20:30:45 +0000

    Delete test.sh

--- test.sh
+++
@@ -1,9 +0,0 @@
-rm ./bin/*.class ./bin/selfish/*.class ./bin/selfish/deck/*.class ./bin/test/*/*.class
-
-javac src/main/*.java src/main/selfish/*.java src/main/selfish/deck/*.java -d ./bin/
-
-javac -cp .;junit-platform-console-standalone.jar --source-path ./src/main/ ./src/test/test/structural/*.java -d ./bin/
-javac -cp .;junit-platform-console-standalone.jar --source-path ./src/main/ ./src/test/test/functional/*.java -d ./bin/
-javac -cp .;junit-platform-console-standalone.jar --source-path ./src/main/ ./src/test/test/javadoc/*.java -d ./bin/
-
-java -jar junit-platform-console-standalone.jar --class-path ./bin/ --scan-class-path --fail-if-no-tests
